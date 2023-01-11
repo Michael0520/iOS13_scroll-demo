7aba637bc44102d2fdc3cd549d8536cb412daa36
Michael0520 <po2319987@gmail.com>
2023-01-12 06:46:20 +0800

    udpate

--- src/main.js
+++ src/main.js
@@ -3,19 +3,33 @@ import "./index.css";
 const button = document.getElementById("button");
 const body = document.body;
 const dialog = document.getElementById("dialog");
+
 let isDialogOpen = false;
+
+const isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
 
 button.addEventListener("click", (e) => {
   e.stopImmediatePropagation();
   if (isDialogOpen) return;
   isDialogOpen = true;
-  body.style.overflow = "hidden";
+  if (isMobile) {
+    body.classList.add("dialog-mobile-open");
+  } else {
+    body.classList.add("dialog-open");
+  }
   dialog.classList.remove("invisible");
 });
 
 body.addEventListener("click", () => {
   if (!isDialogOpen) return;
   isDialogOpen = false;
-  body.style.overflow = "visible";
   dialog.classList.add("invisible");
+  if (isMobile) {
+    body.classList.remove("dialog-mobile-open");
+  } else {
+    body.classList.remove("dialog-open");
+  }
 });
